45bfd9b79c5c3f9d3c5435bd703779139397f8ab
lForger <navajaydan@gmail.com>
2024-06-22 16:25:50 -0400

    Added values for debt and employment

--- pages/js/debt.js
+++ pages/js/debt.js
@@ -6,42 +6,42 @@ const data = {
     datasets: [
     {
       label: 'Software Developer',
-      data: [20000,100000, 70000, 35000, 18000, 10000, 0],
+      data: [16300,100000, 70000, 33000, 19000, 10000, 4000],
      borderColor: "#962DFF",
       fill: false,
       tension: 0.4
     },
     {
       label: 'App Developer',
-      data: [18000,80000, 58000, 25000, 10000, 0, 0, 0],
+      data: [15000,80000, 55000, 30000, 10000, 0, 0],
      borderColor: "#4A3AFF",
       fill: false,
       tension: 0.4
     },
     {
       label: 'Elevator Mechanic',
-      data: [20000,40000,60000,80000,60000,40000,10000],
+      data: [20000,12000,4000,2000,0,0,0],
      borderColor: "#93AAFD",
       fill: false,
       tension: 0.4
     },
     {
       label: 'Electrician',
-      data: [20000,40000,60000,80000,60000,40000,10000],
+      data: [20000,18000,15000,10000,8000,4000,0],
      borderColor: "#D013EE",
       fill: false,
       tension: 0.4
     },
     {
       label: 'Data Scientist',
-      data: [20000,40000,60000,80000,60000,40000,10000],
+      data: [30000,120000,105000,80000,50000,10000,2000],
      borderColor: "#B36CFA",
       fill: false,
       tension: 0.4
     },
     {
       label: 'Surveyer',
-      data: [20000,40000,60000,80000,60000,40000,10000],
+      data: [35000,20000,15000,8000,5000,0,0],
      borderColor: "#7E74EB",
       fill: false,
       tension: 0.4
--- pages/js/employment.js
+++ pages/js/employment.js
@@ -1,21 +1,21 @@
 const soft = [
    {
     label: 'Unemployment',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [28, 24, 25, 36, 15, 13, 12],
     backgroundColor: "#962DFF",
     borderWidth: 1,
     borderRadius: 10,
   },   
    {
     label: 'Underemployment',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [19, 13, 20, 8, 10, 10, 6],
     backgroundColor: "#4A3AFF",
     borderWidth: 1,
     borderRadius: 10,
   },   
    {
     label: 'Employment',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [53, 63, 55, 56, 75, 77, 82],
     backgroundColor: "#93AAFD",
     borderWidth: 1,
     borderRadius: 10,
@@ -25,27 +25,27 @@ const soft = [
 const app = [
    {
     label: 'Unemployment',
-    data: [12, 19, 3, 50, 2, 3],
+    data: [15, 13, 10, 10, 8, 4, 5],
     backgroundColor: "#962DFF",
     borderWidth: 1,
     borderRadius: 10,
   },   
    {
     label: 'Underemployment',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [33, 22, 19, 18, 17, 19, 15],
     backgroundColor: "#4A3AFF",
     borderWidth: 1,
     borderRadius: 10,
   },   
    {
     label: 'Employment',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [52, 65, 71, 72, 75, 77, 80],
     backgroundColor: "#93AAFD",
     borderWidth: 1,
     borderRadius: 10,
   },   
 ];
-const ele = [
+const elev = [
    {
     label: 'Unemployment',
     data: [12, 19, 3, 5, 2, 3],
@@ -114,35 +114,35 @@ const dataS = [
     borderRadius: 10,
   },   
 ];
-const plum = [
+const surv = [
    {
     label: 'Unemployment',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [7, 6, 8, 5, 5, 6, 2],
     backgroundColor: "#962DFF",
     borderWidth: 1,
     borderRadius: 10,
   },   
    {
     label: 'Underemployment',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [5, 2, 2, 4, 5, 8, 7],
     backgroundColor: "#4A3AFF",
     borderWidth: 1,
     borderRadius: 10,
   },   
    {
     label: 'Employment',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [88, 92, 90, 91, 90, 87, 91],
     backgroundColor: "#93AAFD",
     borderWidth: 1,
     borderRadius: 10,
   },   
 ];
 
-const careers = [soft, app, ele, elec, dataS, plum];
+const careers = [soft, app, elev, elec, dataS, surv];
 const ctx = document.getElementById('myChart');
 
 const data = {
-    labels: [ "1010", "2015", "2020", "2025", "2030", "2035", "2040"],
+    labels: [ "2014", "2015", "2016", "2017", "2018", "2019", "2020"],
     datasets: careers[0],
 };
 
