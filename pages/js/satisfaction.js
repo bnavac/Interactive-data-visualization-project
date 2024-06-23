039adf9d6490810317f37cf8c90feabce6ffdcd0
lForger <navajaydan@gmail.com>
2024-06-22 21:11:56 -0400

    updated satisfaction values

--- pages/js/satisfaction.js
+++ pages/js/satisfaction.js
@@ -2,53 +2,53 @@
 const ctx = document.getElementById('myChart');
 
 const data = {
-  labels: ['Application Developer', 'Software Developer', 'Data Scientist', 'Electrician', 'Carpenter', 'Surveyer'],
+  labels: ['Application Developer', 'Software Developer', 'Data Scientist', 'Electrician', 'Carpenter', 'Surveyor'],
   datasets: [
    {
     label: '1',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [4, 12, 12, 20, 15, 12],
     backgroundColor: "#962DFF",
     borderWidth: 1,
     borderRadius: 10,
   },   
    {
     label: '2',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [4, 27, 33, 22, 15, 15],
     backgroundColor: "#4A3AFF",
     borderWidth: 1,
     borderRadius: 10,
   },   
    {
     label: '3',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [4, 25, 22, 22, 15, 12],
     backgroundColor: "#93AAFD",
     borderWidth: 1,
     borderRadius: 10,
   },   
    {
     label: '4',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [6, 8, 10, 10, 15, 8],
     backgroundColor: "#D013EE",
     borderWidth: 1,
     borderRadius: 10,
   },   
    {
     label: '5',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [6, 7, 7, 5, 18, 10],
     backgroundColor: "#B36CFA",
     borderWidth: 1,
     borderRadius: 10,
   },   
    {
     label: '6',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [2, 7, 8, 13, 7, 18],
     backgroundColor: "#7E74EB",
     borderWidth: 1,
     borderRadius: 10,
   },   
    {
     label: '7',
-    data: [12, 19, 3, 5, 2, 3],
+    data: [74, 14, 8, 8, 15, 25],
     backgroundColor: "#1839B0",
     borderWidth: 1,
     borderRadius: 10,
